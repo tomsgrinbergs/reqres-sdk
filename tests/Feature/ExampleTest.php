283d739b3b180c41bf7155bf1de2d2e1da20e301
Toms Grinbergs <toms.grinbergs.lv@gmail.com>
2025-07-06 21:09:19 +0100

    Install PHPUnit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testIsTrue(): void
+    {
+        $this->assertTrue(true);
+    }
+}
